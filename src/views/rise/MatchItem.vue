e91d71f6b01157814e594ddf0d3a026b8af3f6fe
yangmorning <yangchen@cmict.chinamobile.com>
2022-11-06 20:49:58 +0800

    晋级图

--- src/views/rise/MatchItem.vue
+++ src/views/rise/MatchItem.vue
@@ -20,7 +20,7 @@
       <span class="score-">-</span>
       <span>{{matchItem.awayScore}}</span>
     </div>
-    <div v-if="tips" class="tips" :style="`left:calc(50% - ${$vw(tipsWidth/2)});width:${$vw(tipsWidth)};`">
+    <div v-if="tips" class="tips" :style="`left:calc(50% - ${$vw(tipsWidth/2,true)});width:${$vw(tipsWidth,true)};`">
       {{tips}}
     </div>
   </div>
@@ -63,57 +63,114 @@ export default {
 
 <style lang="scss" scoped>
 @import "../../assets/vw";
-.match-item {
-  width: vw(80);
-  height: vw(88);
-  background: rgba(255, 255, 255, 0.06);
-  border-radius: vw(4);
-  padding-top: vw(8);
-  box-sizing: border-box;
-  position: relative;
-  .logo-container{
-    display: flex;
-    justify-content: space-around;
-    font-weight: 500;
-    font-size: vw(12);
-    line-height: vw(24);
-    >div{
-      @extend .ellipsis;
-      flex: 1;
+@media screen and (max-device-width: 600px) {
+  .match-item {
+    width: vw(80);
+    height: vw(88);
+    background: rgba(255, 255, 255, 0.06);
+    border-radius: vw(4);
+    padding-top: vw(8);
+    box-sizing: border-box;
+    position: relative;
+    .logo-container{
+      display: flex;
+      justify-content: space-around;
+      font-weight: 500;
+      font-size: vw(12);
+      line-height: vw(24);
+      >div{
+        overflow:hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        flex: 1;
+        text-align: center;
+      }
+      .logo{
+        display: inline-block;
+        flex: 0 0 vw(24);
+        height: vw(24);
+        margin-bottom: vw(4);
+      }
+    }
+    .score{
+      height: vw(20);
+      font-weight: 500;
+      font-size: vw(12);
+      line-height: vw(24);
+      display: flex;
+      justify-content: center;
+      span{
+        flex: 1;
+      }
+      .score-{
+        flex: 0 0 vw(30);
+        text-align: center;
+      }
+    }
+    .tips{
+      position: absolute;
+      bottom: vw(-14);
       text-align: center;
+      height: vw(20);
+      line-height: vw(20);
+      font-size: vw(12);
+      background: #8E2538;
+      border-radius: vw(20);
     }
-    .logo{
-      display: inline-block;
-      flex: 0 0 vw(24);
-      height: vw(24);
-      margin-bottom: vw(4);
-    }
-  }
-  .score{
-    height: vw(20);
-    font-weight: 500;
-    font-size: vw(12);
-    line-height: vw(24);
-    display: flex;
-    justify-content: center;
-    span{
-      flex: 1;
-    }
-    .score-{
-      flex: 0 0 vw(30);
-      text-align: center;
-    }
-  }
-  .tips{
-    position: absolute;
-    bottom: vw(-14);
-    text-align: center;
-    height: vw(20);
-    line-height: vw(20);
-    font-size: vw(12);
-    background: #8E2538;
-    border-radius: vw(20);
   }
 }
 
+@media screen and (min-device-width: 600px) {
+  .match-item {
+    width: vwPad(160);
+    height: vwPad(88);
+    background: rgba(255, 255, 255, 0.06);
+    border-radius: vwPad(4);
+    padding-top: vwPad(8);
+    box-sizing: border-box;
+    position: relative;
+    .logo-container{
+      display: flex;
+      justify-content: space-around;
+      font-weight: 500;
+      font-size: vwPad(12);
+      line-height: vwPad(24);
+      >div{
+        flex: 1;
+        text-align: center;
+      }
+      .logo{
+        display: inline-block;
+        flex: 0 0 vwPad(24);
+        height: vwPad(24);
+        margin-bottom: vwPad(4);
+      }
+    }
+    .score{
+      height: vwPad(20);
+      font-weight: 500;
+      font-size: vwPad(12);
+      line-height: vwPad(24);
+      display: flex;
+      justify-content: center;
+      span{
+        flex: 1;
+      }
+      .score-{
+        flex: 0 0 vwPad(30);
+        text-align: center;
+      }
+    }
+    .tips{
+      position: absolute;
+      bottom: vwPad(-14);
+      text-align: center;
+      height: vwPad(20);
+      line-height: vwPad(20);
+      font-size: vwPad(12);
+      background: #8E2538;
+      border-radius: vwPad(20);
+    }
+  }
+}
 </style>
