3f97ae387ffa3799a3b2af7a543a0bb84f9a699d
yangmorning <yangchen@cmict.chinamobile.com>
2022-11-08 17:51:45 +0800

    rank

--- src/views/rank/components/rankCard.vue
+++ src/views/rank/components/rankCard.vue
@@ -88,7 +88,9 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import "~@/assets/vw";
-.rank-card {
+
+@media screen and (max-device-width: 600px) {
+  .rank-card {
     background: rgba(0, 0, 0, 0.04);
     border: vw(1) solid rgba(255, 255, 255, 0.12);
     backdrop-filter: blur(#{vw(8)});
@@ -98,63 +100,138 @@ export default {
     font-style: normal;
     color: #ffffff;
     .rank-title {
-        font-weight: 600;
-        font-size: vw(14);
-        line-height: vw(16);
+      font-weight: 600;
+      font-size: vw(14);
+      line-height: vw(16);
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding-bottom: vw(16);
+      .arrow-right {
+        height: vw(16);
+        width: vw(16);
+        background: url("~@/assets/images/arrow-right-small.svg") no-repeat
+        center/contain;
+      }
+    }
+    .rank-content {
+      .rank-content-item {
         display: flex;
         align-items: center;
         justify-content: space-between;
-        padding-bottom: vw(16);
-        .arrow-right {
-            height: vw(16);
-            width: vw(16);
-            background: url("~@/assets/images/arrow-right-small.svg") no-repeat
-                center/contain;
+        padding: vw(8) 0;
+        border-bottom: vw(1) solid rgba(255, 255, 255, 0.12);
+        .item-left {
+          display: flex;
+          align-items: center;
+          .text {
+            padding-left: vw(16);
+            font-weight: 500;
+            font-size: vw(12);
+            line-height: vw(16);
+            .describe {
+              font-weight: 400;
+              transform: scale(0.8);
+              transform-origin: 0 0;
+              line-height: vw(10);
+              color: rgba(255, 255, 255, 0.54);
+            }
+          }
         }
-    }
-    .rank-content {
-        .rank-content-item {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            padding: vw(8) 0;
-            border-bottom: vw(1) solid rgba(255, 255, 255, 0.12);
-            .item-left {
-                display: flex;
-                align-items: center;
-                .text {
-                    padding-left: vw(16);
-                    font-weight: 500;
-                    font-size: vw(12);
-                    line-height: vw(16);
-                    .describe {
-                        font-weight: 400;
-                        transform: scale(0.8);
-                        transform-origin: 0 0;
-                        line-height: vw(10);
-                        color: rgba(255, 255, 255, 0.54);
-                    }
-                }
-            }
-            .score {
-                font-weight: 600;
-                font-size: vw(14);
-                line-height: vw(16);
-                height: vw(24);
-                width: vw(26);
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                font-family: "Inter";
-            }
-            .first {
-                background: #812334;
-                border-radius: 50%;
-            }
+        .score {
+          font-weight: 600;
+          font-size: vw(14);
+          line-height: vw(16);
+          height: vw(24);
+          width: vw(26);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-family: "Inter";
         }
+        .first {
+          background: #812334;
+          border-radius: 50%;
+        }
+      }
     }
     &:not(:last-child) {
-        margin-bottom: vw(16);
+      margin-bottom: vw(16);
     }
+  }
+}
+@media screen and (min-device-width: 600px) {
+  .rank-card {
+    display: inline-block;
+    width: vwPad(327);
+    margin-right: vwPad(16);
+    background: rgba(0, 0, 0, 0.04);
+    border: vwPad(1) solid rgba(255, 255, 255, 0.12);
+    backdrop-filter: blur(#{vwPad(8)});
+    border-radius: vwPad(16);
+    padding: vwPad(16);
+    // font-family: "PingFang SC";
+    font-style: normal;
+    color: #ffffff;
+    .rank-title {
+      font-weight: 600;
+      font-size: vwPad(14);
+      line-height: vwPad(16);
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding-bottom: vwPad(16);
+      .arrow-right {
+        height: vwPad(16);
+        width: vwPad(16);
+        background: url("~@/assets/images/arrow-right-small.svg") no-repeat
+        center/contain;
+      }
+    }
+    .rank-content {
+      .rank-content-item {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: vwPad(8) 0;
+        border-bottom: vwPad(1) solid rgba(255, 255, 255, 0.12);
+        .item-left {
+          display: flex;
+          align-items: center;
+          .text {
+            padding-left: vwPad(16);
+            font-weight: 500;
+            font-size: vwPad(12);
+            line-height: vwPad(16);
+            .describe {
+              font-weight: 400;
+              transform: scale(0.8);
+              transform-origin: 0 0;
+              line-height: vwPad(10);
+              color: rgba(255, 255, 255, 0.54);
+            }
+          }
+        }
+        .score {
+          font-weight: 600;
+          font-size: vwPad(14);
+          line-height: vwPad(16);
+          height: vwPad(24);
+          width: vwPad(26);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-family: "Inter";
+        }
+        .first {
+          background: #812334;
+          border-radius: 50%;
+        }
+      }
+    }
+    &:not(:last-child) {
+      margin-bottom: vwPad(16);
+    }
+  }
 }
 </style>
